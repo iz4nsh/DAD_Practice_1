e6ffebd984caed25cc30b1898803ad07a9bdca92
Iz4nsh <moyagarciaizan@gmail.com>
2025-05-15 14:25:08 +0200

    Initialize Spring Boot project with Maven wrapper, add application properties, and set up basic structure

---
+++ instanceservice/src/main/java/dad/code/instanceservice/InstanceserviceApplication.java
@@ -0,0 +1,13 @@
+package dad.code.instanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstanceserviceApplication.class, args);
+	}
+
+}
---
+++ instanceservice/src/test/java/dad/code/instanceservice/InstanceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dad.code.instanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstanceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
