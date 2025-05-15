111ba7fe89632d300e0d6da34c19e702288a6e35
amdres33 <167109701+amdres33@users.noreply.github.com>
2025-05-15 18:11:06 +0200

    Create DiskRequestListener
    Developed DiskRequestListener to process disk creation via 'disk-requests' queue
      → Simulates disk lifecycle: REQUESTED → INITIALIZING → ASSIGNED
      → Publishes status updates to 'disk-statuses' queue

---
+++ diskservice/src/main/java/es/codeurjc/helloworld_spring/DiskRequestListener.java
@@ -0,0 +1,51 @@
+package es.codeurjc.helloworld_spring;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Component	
+public class DiskRequestListener {
+
+	private final DiskRepository repository;
+	private final RabbitTemplate rabbitTemplate;
+	
+	public DiskRequestListener(DiskRepository repository, RabbitTemplate rabbitTemplate) {
+		super();
+		this.repository = repository;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	@RabbitListener(queues = "disk-requests")
+	public void handle(DiskRequestDto dto) {
+		Disk disk = new Disk(dto.getSize(), Disk.diskType.valueOf(dto.getType().toUpperCase()), Disk.diskStatus.REQUESTED);
+		disk = repository.save(disk);
+		sendStatus(disk);
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor.schedule(() -> {
+            disk.setStatus(Disk.diskStatus.INITIALIZING);
+            repository.save(disk);
+            sendStatus(disk); 
+        }, 5, TimeUnit.SECONDS);
+
+        executor.schedule(() -> {
+            disk.setStatus(Disk.diskStatus.ASSIGNED);
+            repository.save(disk);
+            sendStatus(disk);
+        }, 15, TimeUnit.SECONDS);
+	}
+	
+	private void sendStatus(Disk disk) {
+        DiskStatusDto msg = new DiskStatusDto(
+                disk.getId(),
+                disk.getSize(),
+                disk.getType().name(),
+                disk.getStatus().name()
+        );
+        rabbitTemplate.convertAndSend("disk-statuses", msg);
+    }
+}
