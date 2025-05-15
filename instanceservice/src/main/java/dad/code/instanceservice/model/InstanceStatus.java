134c44460bb231ea542b31ea14b43204e7cd1143
Iz4nsh <moyagarciaizan@gmail.com>
2025-05-15 15:03:13 +0200

    Add InstanceStatus model class with properties for name, status, and IP address

---
+++ instanceservice/src/main/java/dad/code/instanceservice/model/InstanceStatus.java
@@ -0,0 +1,10 @@
+package dad.code.instanceservice.model;
+
+import lombok.Data;
+
+@Data
+public class InstanceStatus {
+    private String name;
+    private String status;
+    private String ip;
+}
