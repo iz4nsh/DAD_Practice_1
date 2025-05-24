753ed977b7b41fe6ccfb43faab53626f8d534b94
amdres33 <167109701+amdres33@users.noreply.github.com>
2025-05-24 19:39:34 +0200

    Update DiskStatus.java
    Update package

--- diskservice/src/main/java/es/codeurjc/diskservice/model/DiskStatus.java
+++ diskservice/src/main/java/es/codeurjc/diskservice/model/DiskStatus.java
@@ -1,12 +1,12 @@
-package es.codeurjc.diskservice.model;
+package dad.code.diskservice.model;
 
 public class DiskStatus {
 	
 	    private long id;
 	    private float size;
-		private String type;
-		private String status;
-		private long instanceId;
+	    private String type;
+	    private String status;
+	    private long instanceId;
 
 	    public DiskStatus() {}
 
@@ -33,25 +33,27 @@ public class DiskStatus {
 	        return status;
 	    }
 
-		public long getInstanceId() {
-			return instanceId;
-		}
+	    public long getInstanceId() {
+		return instanceId;
+	    }
 
-		public void setId(long id) {
-			this.id = id;
-		}
-		public void setSize(float size) {
-			this.size = size;
-		}
-		public void setType(String type) {
-			this.type = type;
-		}
-		public void setStatus(String status) {
-			this.status = status;
-		}
+	    public void setId(long id) {
+		this.id = id;
+	    }
+	
+	    public void setSize(float size) {
+		this.size = size;
+	    }
+	
+	    public void setType(String type) {
+		this.type = type;
+	    }
+	
+	    public void setStatus(String status) {
+		this.status = status;
+  	    }
 		
-		public void setInstanceId(long instanceId) {
-			this.instanceId = instanceId;
-		}
-		
+	    public void setInstanceId(long instanceId) {
+		this.instanceId = instanceId;
+            }
 }
